459fa917a0ba48de7b5731026e513aeb64c130bb
NK8998 <ne.kioko16@gmail.com>
2025-02-01 18:38:36 +0300

    better redirect

--- src/components/loadingscreen/loadingscreen.jsx
+++ src/components/loadingscreen/loadingscreen.jsx
@@ -11,8 +11,14 @@ export const LoadingScreen = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const UrlSCID = window.location.pathname.split("/").find((urlPart) => urlPart.includes("UC"));
-    if (localStorage.getItem("SCID") && UrlSCID && UrlSCID !== JSON.parse(localStorage.getItem("SCID"))) {
+    const UrlSCID = window.location.pathname
+      .split("/")
+      .find((urlPart) => urlPart.includes("UC"));
+    if (
+      localStorage.getItem("SCID") &&
+      UrlSCID &&
+      UrlSCID !== JSON.parse(localStorage.getItem("SCID"))
+    ) {
       document.write("You do not have permission to view this page");
     } else if (!localStorage.getItem("SCID") || !localStorage.getItem("SUID")) {
       setCredentials();
@@ -24,37 +30,47 @@ export const LoadingScreen = () => {
   const verifyCredentials = () => {
     const SCID = JSON.parse(localStorage.getItem("SCID"));
     const SUID = JSON.parse(localStorage.getItem("SUID"));
-    AxiosFetching("get", `Verify-cookie?SCID=${SCID}&SUID=${SUID}`, {}).then((response) => {
-      if (response.data) {
-        if (response.data.redirect === true) {
-          window.location.href = `http://localhost:5174?WAA=Studio`;
-          return;
-        }
-        if (response.data.message !== "verified") {
-          document.write("Token mismatch");
-        } else {
-          const userData = response.data.user_data[0] ? response.data.user_data[0] : {};
-          dispatch(userLoggedIn(userData));
-          if (window.location.pathname === "/") {
-            navigate(`/channel/${SCID}`);
+    AxiosFetching("get", `Verify-cookie?SCID=${SCID}&SUID=${SUID}`, {}).then(
+      (response) => {
+        if (response.data) {
+          if (response.data.redirect === true) {
+            window.location.href = `${import.meta.env.VITE_BASE_URL}?redirect=${
+              window.location.href
+            }`;
+            return;
+          }
+          if (response.data.message !== "verified") {
+            document.write("Token mismatch");
+          } else {
+            const userData = response.data.user_data[0]
+              ? response.data.user_data[0]
+              : {};
+            dispatch(userLoggedIn(userData));
+            if (window.location.pathname === "/") {
+              navigate(`/channel/${SCID}`);
+            }
           }
         }
       }
-    });
+    );
   };
 
   const setCredentials = () => {
     AxiosFetching("get", "Set-cookie", {}).then((response) => {
       if (response.data) {
         if (response.data.redirect === true) {
-          window.location.href = `http://localhost:5174?WAA=Studio`;
+          window.location.href = `${import.meta.env.VITE_BASE_URL}?redirect=${
+            window.location.href
+          }`;
         } else {
           const SCID = response.data.SCID;
           const SUID = response.data.SUID;
           localStorage.setItem("SCID", JSON.stringify(SCID));
           localStorage.setItem("SUID", JSON.stringify(SUID));
 
-          const userData = response.data.user_data[0] ? response.data.user_data[0] : {};
+          const userData = response.data.user_data[0]
+            ? response.data.user_data[0]
+            : {};
           dispatch(userLoggedIn(userData));
           if (window.location.pathname === "/") {
             navigate(`/channel/${SCID}`);
