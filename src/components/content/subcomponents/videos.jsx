6ce7ad3104b63b70534c30591940f59119d64cfc
NK8998 <ne.kioko16@gmail.com>
2025-02-01 17:55:39 +0300

    case!! sensitivity

--- src/components/content/subcomponents/videos.jsx
+++ src/components/content/subcomponents/videos.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUsersVideos } from "../../../store/Videos-slice";
-import TableComponent from "./Utilities/table/table";
+import TableComponent from "./utilities/table/table";
 import "./css/video.css";
 
 export const Videos = () => {
